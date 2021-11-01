66ac3aa88ba4796c80dbf77c1e9dc56bc5e2a54c
moritz <moritz@moritz-desktop>
2021-11-01 13:24:43 +0100

    synology drive

--- tweaks/synology-drive/synology-drive.sh
+++ tweaks/synology-drive/synology-drive.sh
@@ -11,9 +11,14 @@ remove() {
 }
 
 update() {
-    curl -L "$TWEAK_RES_URL/cloud-sync.png" -o $HOME/.SynologyDrive/SynologyDrive.app/images/tray/win-linux-black/syncing.png
-    curl -L "$TWEAK_RES_URL/cloud-normal.png" -o $HOME/.SynologyDrive/SynologyDrive.app/images/tray/win-linux-black/uptodate.png
-    curl -L "$TWEAK_RES_URL/cloud-normal.png" -o $HOME/.SynologyDrive/SynologyDrive.app/images/tray/win-linux-black/normal.png
+    local drive_icons_dir="$HOME/.SynologyDrive/SynologyDrive.app/images/tray/win-linux-black"
+    curl -L "$TWEAK_RES_URL/syncing.png" -o "$drive_icons_dir/syncing.png"
+    curl -L "$TWEAK_RES_URL/uptodate.png" -o "$drive_icons_dir/uptodate.png"
+    curl -L "$TWEAK_RES_URL/normal.png" -o "$drive_icons_dir/normal.png"
+    curl -L "$TWEAK_RES_URL/abnormal.png" -o "$drive_icons_dir/abnormal.png"
+    curl -L "$TWEAK_RES_URL/disconnect.png" -o "$drive_icons_dir/disconnect.png"
+    curl -L "$TWEAK_RES_URL/notification.png" -o "$drive_icons_dir/notification.png"
+    curl -L "$TWEAK_RES_URL/pause.png" -o "$drive_icons_dir/pause.png"
 }
 
 if [ "$1" = "install" ]; then install "$@"; fi
