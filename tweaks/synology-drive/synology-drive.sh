03675a0c8d73a9523ed0c9ec52fa340d1e718dbc
moritz <moritz@moritz-desktop>
2021-11-07 12:12:09 +0100

    check

--- tweaks/synology-drive/synology-drive.sh
+++ tweaks/synology-drive/synology-drive.sh
@@ -14,13 +14,15 @@ remove() {
 
 update() {
     local drive_icons_dir="$HOME/.SynologyDrive/SynologyDrive.app/images/tray/normal"
-    cp -f "$TWEAK_RES_DIR/syncing.png" "$drive_icons_dir/syncing.png"
-    cp -f "$TWEAK_RES_DIR/uptodate.png" "$drive_icons_dir/uptodate.png"
-    cp -f "$TWEAK_RES_DIR/normal.png" "$drive_icons_dir/normal.png"
-    cp -f "$TWEAK_RES_DIR/abnormal.png" "$drive_icons_dir/abnormal.png"
-    cp -f "$TWEAK_RES_DIR/disconnect.png" "$drive_icons_dir/disconnect.png"
-    cp -f "$TWEAK_RES_DIR/notification.png" "$drive_icons_dir/notification.png"
-    cp -f "$TWEAK_RES_DIR/pause.png" "$drive_icons_dir/pause.png"
+    if [ -d "$drive_icons_dir" ]; then
+        cp -f "$TWEAK_RES_DIR/syncing.png" "$drive_icons_dir/syncing.png"
+        cp -f "$TWEAK_RES_DIR/uptodate.png" "$drive_icons_dir/uptodate.png"
+        cp -f "$TWEAK_RES_DIR/normal.png" "$drive_icons_dir/normal.png"
+        cp -f "$TWEAK_RES_DIR/abnormal.png" "$drive_icons_dir/abnormal.png"
+        cp -f "$TWEAK_RES_DIR/disconnect.png" "$drive_icons_dir/disconnect.png"
+        cp -f "$TWEAK_RES_DIR/notification.png" "$drive_icons_dir/notification.png"
+        cp -f "$TWEAK_RES_DIR/pause.png" "$drive_icons_dir/pause.png"
+    fi
 }
 
 if [ "$1" = "install" ]; then install "$@"; fi
