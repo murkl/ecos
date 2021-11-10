624f9af1c814c808e723de867bc4a7fbc70276f4
moritz <moritz@moritz-desktop>
2021-11-10 14:22:38 +0100

    sleep

--- tweaks/gnome-extension-dash_to_panel/gnome-extension-dash_to_panel.sh
+++ tweaks/gnome-extension-dash_to_panel/gnome-extension-dash_to_panel.sh
@@ -9,6 +9,8 @@ install() {
     cd "$TWEAK_CACHE_DIR/repo" || exit 1
     make install
     gnome-extensions enable "dash-to-panel@jderose9.github.com"
+    killall -3 gnome-shell
+    sleep 3
 }
 
 remove() {
@@ -20,6 +22,7 @@ update() {
     git pull
     remove
     make install
+
 }
 
 if [ "$1" = "install" ]; then install "$@"; fi
