dee38af56a504d33322f0b0eddb352fc31de916f
moritz <moritz@moritz-desktop>
2021-11-21 14:20:33 +0100

    intel

--- tweaks/xf86-video-intel/xf86-video-intel.sh
+++ tweaks/xf86-video-intel/xf86-video-intel.sh
@@ -19,12 +19,14 @@ install() {
     paru --noconfirm --needed --sudoloop -S xf86-video-intel mesa vulkan-intel vulkan-tools
     paru --noconfirm --needed --sudoloop -S lib32-mesa lib32-vulkan-intel
     sudo sed -i "s/MODULES=()/MODULES=(intel_agp i915)/g" "/etc/mkinitcpio.conf"
+    echo 'COGL_ATLAS_DEFAULT_BLIT_MODE=framebuffer' | sudo tee -a "/etc/environment"
     sudo mkinitcpio -p linux
 }
 
 remove() {
     paru --noconfirm --sudoloop -R xf86-video-intel vulkan-intel vulkan-tools lib32-vulkan-intel
     sudo sed -i "s/MODULES=(intel_agp i915)/MODULES=()/g" "/etc/mkinitcpio.conf"
+    sudo sed -i '/COGL_ATLAS_DEFAULT_BLIT_MODE=framebuffer/d' "/etc/environment"
     sudo mkinitcpio -p linux
 }
 
