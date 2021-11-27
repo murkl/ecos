8d3910ebc122edfe78a07c4ab8370e0211ef0ed1
moritz <moritz@moritz-notebook>
2021-11-27 19:25:10 +0100

    deploy

--- dev.sh
+++ dev.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 SCRIPT_DIR="$(cd "$(dirname "$0")" >/dev/null 2>&1 && pwd)"
 
 # DEV FILE
@@ -15,22 +14,35 @@ VERSION_RELEASE="$($SCRIPT_RELEASE --version)"
 # //////////////////////////////////
 
 init() {
+    echo -e "////////////////////////"
+    echo -e "> ECOS CORE DEVELOPMENT"
+    echo -e "////////////////////////"
     echo -e "\n"
-    echo -e "//////////////////////////"
-    echo -e "> DEPLOY ECOS CORE"
-    echo -e "//////////////////////////"
-    echo -e "\n"
-    echo -e "VERSION RELEASE:  $VERSION_RELEASE"
     echo -e "VERSION DEV:      $VERSION_DEV"
-    echo -e "\n\n"
+    echo -e "VERSION RELEASE:  $VERSION_RELEASE"
+    echo -e "\n"
     read -p "DEPLOY ECOS $VERSION_DEV? [yes/no]: " user_input
     if [ "$user_input" = "yes" ]; then
+
+        # Check diff version
+        if [ "$VERSION_DEV" = "$VERSION_RELEASE" ]; then
+            echo -e "\n!! SAME VERSION !!\n"
+            read -p "CONTINUE? [y/n]: " user_input
+            if [ "$user_input" != "y" ]; then
+                echo -e "Skipped..."
+                exit 0
+            fi
+        fi
+
+        # Copy dev to release
         if ! cp -f "$SCRIPT_DEV" "$SCRIPT_RELEASE"; then
             echo -e "ERROR: cp -f "$SCRIPT_DEV" "$SCRIPT_RELEASE""
             exit 1
         fi
+
+        # Git commit
         cd "$SCRIPT_DIR" || exit 1
-        git add "$SCRIPT_RELEASE" && git commit -m "$($SCRIPT_RELEASE --version)"
+        git add "./release/ecos" && git commit -m "$(./release/ecos --version)"
         echo -e "ECOS CORE succefully deployed!"
     else
         echo -e "Skipped..."
