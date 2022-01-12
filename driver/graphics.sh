acf889563502bd175d98fb13505f42fc31f442b3
moritz <moritz@moritz-desktop>
2022-01-12 01:59:33 +0100

    pw test

--- driver/graphics.sh
+++ driver/graphics.sh
@@ -7,20 +7,18 @@ MENU_ITEMS+=("NVIDIA Graphics Driver")
 MENU_ITEMS+=("AMD Graphics Driver")
 
 main() {
-    if ! ZENITY_RESULT="$(ecos --api menu "$TITLE" "${MENU_ITEMS[@]}")"; then
+    if ! local ZENITY_RESULT="$(ecos --api menu "$TITLE" "${MENU_ITEMS[@]}")"; then
         exit 0
     fi
 
-    if ! ROOT_PASSWORD="$(ecos --api root)"; then
+    if ! local ROOT_PASSWORD="$(ecos --api root)"; then
         exit 0
     fi
 
-    if ! ecos --api check-root; then
+    if ! ecos --api check-root "$ROOT_PASSWORD"; then
         ecos --api notify "Root Password wrong!"
         exit 1
     fi
-
-    sh -c "echo $root_password | sudo -S cp /tmp/$SCRIPT_ID.systemd $SYSTEMD_SERVICE_FILE"
 
     if [ "$ZENITY_RESULT" = "Intel Graphics Driver" ]; then
         ecos --api notify "$ROOT_PASSWORD"
@@ -34,6 +32,8 @@ main() {
     if [ "$ZENITY_RESULT" = "AMD Graphics Driver" ]; then
         exit 0
     fi
+
+    ROOT_PASSWORD=''
 }
 
 main "$@"
